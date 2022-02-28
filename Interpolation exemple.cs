f47e86dc967434fbd0f6358b0c5604345ea5bc5d
FrCode91 <73969123+Francieine@users.noreply.github.com>
2022-02-28 18:14:59 -0300

    Add files via upload

---
+++ Interpolation exemple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Interpolations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Exemple of interpolation with strings//
+
+            string fullname = "Fran Sabino";
+            string subjects = "Math, English, Portuguese, Grammar";
+            string grades = "A, B, C , D";
+            string fullInfo = $"Full name: {fullname}, \rSubjects: {subjects}, \rYour grades is: {grades}";
+            Console.WriteLine(fullInfo);
+        }
+    }
+}
